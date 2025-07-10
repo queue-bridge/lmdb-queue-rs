8ae3c165b619423f43a1670a3fa49487bb6f0d7d
Tal <talrasha007@gmail.com>
2025-07-10 19:37:48 +0800

    init Topic.

---
+++ src/env.rs
@@ -0,0 +1,41 @@
+use std::path::Path;
+use lmdb::{Environment, RoTransaction, RwTransaction, EnvironmentFlags, Error};
+use libc::{c_uint, size_t};
+
+pub struct Env {
+    lmdb_env: lmdb::Environment,
+}
+
+impl Env {
+    pub fn new<P: AsRef<Path>>(root: P, max_topics: Option<c_uint>, map_size: Option<size_t>) -> Result<Env, Error> {
+        Environment::new()
+            .set_map_size(map_size.unwrap_or(256 * 1024 * 1024))
+            .set_max_dbs(max_topics.unwrap_or(256))
+            .set_flags(EnvironmentFlags::NO_SYNC | EnvironmentFlags::NO_SUB_DIR)
+            .open(root.as_ref())
+            .map(|lmdb_env| Env { lmdb_env })
+    }
+
+    pub fn topic(&self, name: &str) -> Result<super::topic::Topic, Error> {
+        super::topic::Topic::new(&self, name)
+    }
+
+    pub fn transaction_ro(&self) -> Result<RoTransaction, Error> {
+        self.lmdb_env.begin_ro_txn()
+    }
+
+    pub fn transaction_rw(&self) -> Result<RwTransaction, Error> {
+        self.lmdb_env.begin_rw_txn()
+    }
+}
+
+#[test]
+fn test_env_new() {
+    let env = Env::new("/tmp/foo", None, None);
+    assert!(env.is_ok(), "Expected Ok, got Err({:?})", env.err());
+
+    if let Ok(env) = env {
+        let topic = env.topic("bar");
+        assert!(topic.is_ok(), "Expected Ok, got Err({:?})", topic.err());
+    }
+}
---
+++ src/topic.rs
@@ -0,0 +1,88 @@
+use lmdb::{Database, Error, DatabaseFlags, Transaction, WriteFlags};
+use lmdb_sys::{MDB_val, mdb_set_compare};
+use libc::{c_int, memcmp};
+use super::env::Env;
+
+static KEY_PRODUCER: &'static [u8] = "producer_head".as_bytes();
+
+fn slice_to_u64(slice: &[u8]) -> Result<u64, Error> {
+    let arr: [u8; 8] = slice.try_into().map_err(|_| Error::Corrupted)?;
+    Ok(u64::from_ne_bytes(arr))
+}
+
+fn u64_to_bytes(v: u64) -> [u8; 8] {
+    v.to_ne_bytes()
+}
+
+pub struct Topic<'env> {
+    env: &'env Env,
+    db: Database,
+}
+
+extern "C" fn mdb_int_cmp_u32(a: *const MDB_val, b: *const MDB_val) -> c_int {
+    unsafe {
+        let a_val = *( (*a).mv_data as *const u32 );
+        let b_val = *( (*b).mv_data as *const u32 );
+        a_val.cmp(&b_val) as c_int  // reverse for DESC
+    }
+}
+
+extern "C" fn mdb_int_cmp_u64(a: *const MDB_val, b: *const MDB_val) -> c_int {
+    unsafe {
+        let a_val = *( (*a).mv_data as *const u64 );
+        let b_val = *( (*b).mv_data as *const u64 );
+        a_val.cmp(&b_val) as c_int  // reverse for DESC
+    }
+}
+
+extern "C" fn desc_cmp(a: *const MDB_val, b: *const MDB_val) -> c_int {
+    unsafe {
+        let a_size = (*a).mv_size;
+        let b_size = (*b).mv_size;
+
+        // DESC order by size
+        if a_size > b_size {
+            return -1;
+        }
+
+        if a_size < b_size {
+            return 1;
+        }
+
+        match a_size {
+            s if s == size_of::<u32>() => mdb_int_cmp_u32(a, b),
+            s if s == size_of::<u64>() => mdb_int_cmp_u64(a, b),
+            _ => memcmp((*a).mv_data, (*b).mv_data, a_size)
+        }
+    }
+}
+
+impl<'env> Topic<'env> {
+    pub fn new(env: &'env Env, name: &str) -> Result<Topic<'env>, Error>  {
+        let mut txn = env.transaction_rw()?;
+        let db = unsafe { txn.create_db(Some(name), DatabaseFlags::empty())? };
+        unsafe {
+            mdb_set_compare(txn.txn(), db.dbi(), desc_cmp as *mut _);
+        }
+
+        if let Ok(_) = txn.put(db, &KEY_PRODUCER, &u64_to_bytes(0), WriteFlags::NO_OVERWRITE) {
+
+        }
+
+        txn.commit()?;
+
+        Ok(Topic { env, db })
+    }
+
+    pub fn get_producer_head(&self) -> Result<u64, Error> {
+        self.get_producer_head_with_txn(&self.env.transaction_ro()?)
+    }
+
+
+    pub fn get_producer_head_with_txn<TXN>(&self, txn: &TXN) -> Result<u64, Error>
+    where
+        TXN: Transaction
+    {
+        slice_to_u64(txn.get(self.db, &KEY_PRODUCER)?)
+    }
+}
