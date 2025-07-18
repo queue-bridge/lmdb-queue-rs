3cdc57dbbe0643612675ee3b6fc7547046dfd689
Tal <talrasha007@gmail.com>
2025-07-18 15:45:06 +0800

    consumer & producer create & free.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,12 +1,5 @@
 use super::env::Env;
-use super::topic::{Producer, Comsumer};
-
-#[repr(C)]
-pub enum ErrorCode {
-    Ok = 0,
-    NullPtr = 1,
-    CreationFailed = 2,
-}
+use super::topic::{Producer, Consumer};
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn queue_env_new(
@@ -36,5 +29,67 @@ pub unsafe extern "C" fn queue_env_new(
 pub unsafe extern "C" fn queue_env_free(env: *mut Env) {
     if !env.is_null() {
         unsafe { drop(Box::from_raw(env)); }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn queue_consumer_new(
+    env: *mut Env,
+    name: *const libc::c_char,
+    chunks_to_keep: u64,
+) -> *mut Consumer<'static> {
+    assert!(!env.is_null());
+    assert!(!name.is_null());
+
+    let env: &mut Env = unsafe { &mut *env };
+    let name = unsafe { std::ffi::CStr::from_ptr(name) };
+    
+    match env.consumer(
+        &name.to_string_lossy(),
+        if chunks_to_keep == 0 { None } else { Some(chunks_to_keep) },
+    ) {
+        Ok(consumer) => Box::into_raw(Box::new(consumer)),
+        Err(e) => {
+            eprintln!("queue_env_new error: {:?}", e);
+            std::ptr::null_mut()
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn queue_consumer_free(consumer: *mut Consumer) {
+    if !consumer.is_null() {
+        unsafe { drop(Box::from_raw(consumer)); }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn queue_producer_new(
+    env: *mut Env,
+    name: *const libc::c_char,
+    chunk_size: u64,
+) -> *mut Producer<'static> {
+    assert!(!env.is_null());
+    assert!(!name.is_null());
+
+    let env: &mut Env = unsafe { &mut *env };
+    let name = unsafe { std::ffi::CStr::from_ptr(name) };
+    
+    match env.producer(
+        &name.to_string_lossy(),
+        if chunk_size == 0 { None } else { Some(chunk_size) },
+    ) {
+        Ok(producer) => Box::into_raw(Box::new(producer)),
+        Err(e) => {
+            eprintln!("queue_env_new error: {:?}", e);
+            std::ptr::null_mut()
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn queue_producer_free(producer: *mut Producer) {
+    if !producer.is_null() {
+        unsafe { drop(Box::from_raw(producer)); }
     }
 }
