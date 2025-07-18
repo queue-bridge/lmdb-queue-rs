7dcbdea951027dd87b8a4193ee7e91330b773276
Tal <talrasha007@gmail.com>
2025-07-18 15:24:37 +0800

    return *mut Env.

--- src/ffi.rs
+++ src/ffi.rs
@@ -13,29 +13,21 @@ pub unsafe  extern "C" fn queue_env_new(
     root: *const libc::c_char,
     max_topics: libc::c_uint,
     map_size: libc::size_t,
-    env_out: *mut *mut Env,
-) -> ErrorCode {
-    if env_out.is_null() { return ErrorCode::NullPtr; }
-
-    let cstr = if root.is_null() {
-        return ErrorCode::NullPtr;
-    } else {
-        unsafe { std::ffi::CStr::from_ptr(root) }
+) -> *mut Env {
+    let root = unsafe {
+        assert!(!root.is_null());
+        std::ffi::CStr::from_ptr(root)
     };
-
+    
     match Env::new(
-        std::path::PathBuf::from(cstr.to_string_lossy().into_owned()),
+        std::path::PathBuf::from(root.to_string_lossy().into_owned()),
         if max_topics == 0 { None } else { Some(max_topics) },
         if map_size == 0 { None } else { Some(map_size) },
     ) {
-        Ok(env) => {
-            let boxed = Box::new(env);
-            unsafe { *env_out = Box::into_raw(boxed); }
-            ErrorCode::Ok
-        }
-        Err(_) => {
-            unsafe { *env_out = std::ptr::null_mut(); }
-            ErrorCode::CreationFailed
+        Ok(env) => Box::into_raw(Box::new(env)),
+        Err(e) => {
+            eprintln!("queue_env_new error: {:?}", e);
+            std::ptr::null_mut()
         }
     }
 }
