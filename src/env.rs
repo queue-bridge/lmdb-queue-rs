1ef37031031178f881465d72dc8437589491c8c0
Tal <talrasha007@gmail.com>
2025-07-28 21:07:39 +0800

    use heed3.

--- src/env.rs
+++ src/env.rs
@@ -1,6 +1,8 @@
+use std::error::Error;
 use std::path::Path;
-use lmdb::{Environment, RoTransaction, RwTransaction, EnvironmentFlags, Error};
 use libc::{c_uint, size_t};
+
+use heed3::{Database, EnvFlags, EnvOpenOptions, RoTxn, RwTxn, WithTls};
 
 use super::topic::{Consumer, Producer};
 
@@ -8,39 +10,49 @@ use super::topic::{Consumer, Producer};
 use super::topic::Topic;
 
 pub struct Env {
-    pub lmdb_env: lmdb::Environment,
+    pub lmdb_env: heed3::Env,
     pub root: String,
 }
 
 impl Env {
-    pub fn new<P: AsRef<Path>>(root: P, max_topics: Option<c_uint>, map_size: Option<size_t>) -> Result<Env, Error> {
-        Environment::new()
-            .set_map_size(map_size.unwrap_or(256 * 1024 * 1024))
-            .set_max_dbs(max_topics.unwrap_or(256))
-            .set_flags(EnvironmentFlags::NO_SYNC | EnvironmentFlags::NO_TLS | EnvironmentFlags::NO_SUB_DIR)
-            .open(root.as_ref())
-            .map(|lmdb_env| Env { lmdb_env, root: root.as_ref().to_str().unwrap().to_string() })
+    pub fn new<P: AsRef<Path>>(root: P, max_topics: Option<c_uint>, map_size: Option<size_t>) -> Result<Env, Box<dyn Error>> {
+        let env = unsafe {
+            EnvOpenOptions::new()
+                .map_size(map_size.unwrap_or(256 * 1024 * 1024))
+                .max_dbs(max_topics.unwrap_or(256) * 2)
+                .flags(EnvFlags::NO_SYNC | EnvFlags::NO_SUB_DIR)
+                .open(root.as_ref())
+                .map(|lmdb_env| Env { lmdb_env, root: root.as_ref().to_str().unwrap().to_string() })
+        };
+
+        Ok(env?)
     }
 
-    pub fn producer(&self, name: &str, chunk_size: Option<u64>) -> Result<super::topic::Producer, anyhow::Error> {
+    pub fn db<K, V>(&self, wtxn: &mut RwTxn, name: &str) -> Result<Database<K, V>, Box<dyn Error>>
+    where K: 'static, V: 'static
+    {
+        Ok(self.lmdb_env.open_database::<K, V>(wtxn, Some(name))?.unwrap())
+    }
+
+    pub fn producer(&self, name: &str, chunk_size: Option<u64>) -> Result<Producer, Box<dyn Error>> {
         Producer::new(&self, name, chunk_size)
     }
 
-    pub fn consumer(&self, name: &str, chunks_to_keep: Option<u64>) -> Result<super::topic::Consumer, anyhow::Error> {
+    pub fn consumer(&self, name: &str, chunks_to_keep: Option<u64>) -> Result<Consumer, Box<dyn Error>> {
         Consumer::new(&self, name, chunks_to_keep)
     }
 
-    pub fn transaction_ro(&self) -> Result<RoTransaction, Error> {
-        self.lmdb_env.begin_ro_txn()
+    pub fn write_txn(&self) -> Result<RwTxn, Box<dyn Error>> {
+        Ok(self.lmdb_env.write_txn()?)
     }
 
-    pub fn transaction_rw(&self) -> Result<RwTransaction, Error> {
-        self.lmdb_env.begin_rw_txn()
+    pub fn read_txn(&self) -> Result<RoTxn<WithTls>, Box<dyn Error>> {
+        Ok(self.lmdb_env.read_txn()?)
     }
 }
 
 #[test]
-fn test_single() -> Result<(), anyhow::Error> {
+fn test_single() -> Result<(), Box<dyn Error>> {
     let env = Env::new("/tmp/foo_env", None, None)?;
     let mut producer = env.producer("test", Some(16 *1024 * 1024))?;
     for i in 0..1024*1024 {
@@ -71,7 +83,7 @@ fn test_single() -> Result<(), anyhow::Error> {
 }
 
 #[test]
-fn test_batch() -> Result<(), anyhow::Error> {
+fn test_batch() -> Result<(), Box<dyn Error>> {
     let env = Env::new("/tmp/foo_env", None, None)?;
     let mut producer = env.producer("test", Some(16 * 1024 * 1024))?;
     for i in 0..1024*100 {
--- src/topic.rs
+++ src/topic.rs
@@ -1,76 +1,40 @@
-use lmdb::{Cursor, Database, DatabaseFlags, Error, RwTransaction, Transaction, WriteFlags};
-use lmdb_sys::{MDB_LAST, MDB_SET_RANGE};
+use std::error::Error;
+use heed3::byteorder::BE;
+use heed3::types::*;
+use heed3::{RwTxn, Database, PutFlags};
+
 use super::env::Env;
 
 use super::reader::{Reader, Item};
 use super::writer::Writer;
 
-pub static KEY_CONSUMER_FILE: [u8; 1] = [0];
-pub static KEY_CONSUMER_OFFSET: [u8; 2] = [0, 0];
-pub static KEY_CONSUMER_BYTES_READ: [u8; 3] = [0, 0, 0];
-
-pub fn slice_to_u64(slice: &[u8]) -> Result<u64, Error> {
-    let arr: [u8; 8] = slice.try_into().map_err(|_| Error::Corrupted)?;
-    Ok(u64::from_be_bytes(arr))
-}
-
-pub fn u64_to_bytes(v: u64) -> [u8; 8] {
-    v.to_be_bytes()
-}
+pub static KEY_CONSUMER_FILE: &str = "FILE";
+pub static KEY_CONSUMER_OFFSET: &str = "OFFSET";
+pub static KEY_CONSUMER_BYTES_READ: &str = "BYTES_READ";
 
 pub trait Topic {
     fn get_env(&self) -> &Env;
-    fn get_db(&self) -> Database;
+    fn get_producer_db(&self) -> Database<U64<BE>, U64<BE>>;
+    fn get_consumer_db(&self) -> Database<Str, U64<BE>>;
 
-    fn lag(&self) -> Result<u64, Error> {
-        let txn = self.get_env().transaction_ro()?;
-        let db = self.get_db();
-        let head = Self::get_value(db, &txn, &KEY_CONSUMER_FILE)?;
-        let (tail, _) = Self::get_tail(db, &txn)?;
-        let total = (head..tail + 1)
-            .map(|v| Self::get_value(db, &txn, &u64_to_bytes(v)).unwrap_or(0))
-            .reduce(|acc, v| acc + v)
-            .unwrap_or(0);
+    fn lag(&self) -> Result<u64, Box<dyn Error>> {
+        let txn = self.get_env().write_txn()?;
 
-        let head_offset = Self::get_value(db, &txn, &KEY_CONSUMER_OFFSET)?;
-        Ok(total - head_offset)
-    }
-
-    fn inc(&self, txn: &mut RwTransaction, key: &[u8], delta: u64) -> Result<(), Error> {
-        let mut cur = txn.open_rw_cursor(self.get_db())?;
-        let (_, old) = cur.get(Some(&key), None, MDB_SET_RANGE)?;
-        let old_val = slice_to_u64(old)?;
-        cur.put(&key, &u64_to_bytes(old_val + delta), WriteFlags::CURRENT)
-    }
-
-    fn replace(&self, txn: &mut RwTransaction, key: &[u8], value: u64) -> Result<(), Error> {
-        let mut cur = txn.open_rw_cursor(self.get_db())?;
-        cur.get(Some(&key), None, MDB_SET_RANGE)?;
-        cur.put(&key, &u64_to_bytes(value), WriteFlags::CURRENT)
-    }
-
-    fn get_tail<TXN>(db: Database, txn: &TXN) -> Result<(u64, u64), Error>
-    where TXN: Transaction
-    {
-        let cur = txn.open_ro_cursor(db)?;
-        if let (Some(key), value) = cur.get(None, None, MDB_LAST)? {
-            Ok((slice_to_u64(key)?, slice_to_u64(value)?))
-        } else {
-            Err(Error::NotFound)
+        let mut pit = self.get_producer_db().iter(&txn)?.move_between_keys();
+        let mut total: u64 = 0;
+        while let Some((_, v)) = pit.next().transpose()? {
+            total += v;
         }
-    }
 
-    fn get_value<TXN>(db: Database, txn: &TXN, key: &[u8]) -> Result<u64, Error>
-    where TXN: Transaction
-    {
-        let value = txn.get(db, &key)?;
-        slice_to_u64(value)
+        let head_offset = self.get_consumer_db().get(&txn, KEY_CONSUMER_OFFSET)?.unwrap_or(0);
+        Ok(total - head_offset)
     }
 }
 
 pub struct Producer<'env> {
     env: &'env Env,
-    db: Database,
+    producer_db: Database<U64<BE>, U64<BE>>,
+    consumer_db: Database<Str, U64<BE>>,
     writer: Writer,
     chunk_size: u64,
 }
@@ -80,55 +44,61 @@ impl<'env> Topic for Producer<'env> {
         self.env
     }
 
-    fn get_db(&self) -> Database {
-        self.db
+    fn get_producer_db(&self) -> Database<U64<BE>, U64<BE>> {
+        self.producer_db
+    }
+
+    fn get_consumer_db(&self) -> Database<Str, U64<BE>> {
+        self.consumer_db
     }
 }
 
 impl<'env> Producer<'env> {
-    pub fn new(env: &'env Env, name: &str, chunk_size: Option<u64>) -> Result<Self, anyhow::Error> {
-        let mut txn = env.transaction_rw()?;
-        let db = unsafe { txn.create_db(Some(name), DatabaseFlags::empty())? };
+    pub fn new(env: &'env Env, name: &str, chunk_size: Option<u64>) -> Result<Self, Box<dyn Error>> {
+        let mut txn = env.write_txn()?;
+        let producer_db: Database<U64<BE>, U64<BE>> = env.db(&mut txn, &format!("{}_{}", name, "producer"))?;
+        let consumer_db: Database<Str, U64<BE>> = env.db(&mut txn, &format!("{}_{}", name, "consumer"))?;
 
-        let zero = &u64_to_bytes(0);
-        if let Ok(_) = txn.put(db, &KEY_CONSUMER_FILE, zero, WriteFlags::NO_OVERWRITE) {
-            txn.put(db, &KEY_CONSUMER_OFFSET, zero, WriteFlags::NO_OVERWRITE)?;
-            txn.put(db, &KEY_CONSUMER_BYTES_READ, zero, WriteFlags::NO_OVERWRITE)?;
-            txn.put(db, zero, zero, WriteFlags::NO_OVERWRITE)?;
+        if let Ok(_) = consumer_db.put_with_flags(&mut txn, PutFlags::NO_OVERWRITE, &KEY_CONSUMER_FILE, &0) {
+            producer_db.put_with_flags(&mut txn, PutFlags::NO_OVERWRITE, &0, &0)?;
+            consumer_db.put_with_flags(&mut txn, PutFlags::NO_OVERWRITE, &KEY_CONSUMER_OFFSET, &0)?;
+            consumer_db.put_with_flags(&mut txn, PutFlags::NO_OVERWRITE, &KEY_CONSUMER_BYTES_READ, &0)?;
         }
 
-        let (tail_file, _) = Self::get_tail(db, &txn)?;
+        let (tail_file, _) = producer_db.iter(&txn)?.last().transpose()?.unwrap();
         let writer = Writer::new(&env.root, name, tail_file)?;
 
         txn.commit()?;
 
-        Ok(Producer { env, db, writer, chunk_size: chunk_size.unwrap_or(64 * 1024 * 1024) })
+        Ok(Producer { env, producer_db, consumer_db, writer, chunk_size: chunk_size.unwrap_or(64 * 1024 * 1024) })
     }
 
-    pub fn push_back_batch<'a, B>(&mut self, messages: &'a B) -> Result<(), anyhow::Error>
+    pub fn push_back_batch<'a, B>(&mut self, messages: &'a B) -> Result<(), Box<dyn Error>>
     where B: AsRef<[&'a [u8]]>
     {
-        let mut txn = self.env.transaction_rw()?;
-        let (mut tail_file, _) = Self::get_tail(self.db, &txn)?;
+        let mut txn = self.env.write_txn()?;
+        let (mut tail_file, mut offset) = self.producer_db.iter(&txn)?.last().transpose()?.unwrap();
         if self.writer.file_size()? > self.chunk_size {
             self.writer.rotate()?;
             tail_file += 1;
-            txn.put(self.db, &u64_to_bytes(tail_file), &u64_to_bytes(0), WriteFlags::empty())?;
+            offset = 0;
+            self.producer_db.put(&mut txn, &tail_file, &0)?;
         }
         self.writer.put_batch(messages)?;
-        self.inc(&mut txn, &u64_to_bytes(tail_file), messages.as_ref().len() as u64)?;
+        self.producer_db.put(&mut txn, &tail_file, &(offset + messages.as_ref().len() as u64))?;
         txn.commit()?;
         Ok(())
     }
 
-    pub fn push_back<'a>(&mut self, message: &'a [u8]) -> Result<(), anyhow::Error> {
+    pub fn push_back<'a>(&mut self, message: &'a [u8]) -> Result<(), Box<dyn Error>> {
         self.push_back_batch(&[message])
     }
 }
 
 pub struct Consumer<'env> {
     env: &'env Env,
-    db: Database,
+    producer_db: Database<U64<BE>, U64<BE>>,
+    consumer_db: Database<Str, U64<BE>>,
     reader: Reader,
     chunks_to_keep: u64,
 }
@@ -138,18 +108,23 @@ impl <'env> Topic for Consumer<'env> {
         self.env
     }
 
-    fn get_db(&self) -> Database {
-        self.db
-    }    
+    fn get_producer_db(&self) -> Database<U64<BE>, U64<BE>> {
+        self.producer_db
+    }
+
+    fn get_consumer_db(&self) -> Database<Str, U64<BE>> {
+        self.consumer_db
+    }
 }
 
 impl <'env> Consumer<'env> {
-    pub fn new(env: &'env Env, name: &str, chunks_to_keep: Option<u64>) -> Result<Self, anyhow::Error> {
-        let txn = env.transaction_ro()?;
-        let db = unsafe { txn.open_db(Some(name))? };
-        // let offset = Self::get_value(db, &txn, &KEY_CONSUMER_OFFSET)?;
-        let bytes_read = Self::get_value(db, &txn, &KEY_CONSUMER_BYTES_READ)?;
-        let file_num = Self::get_value(db, &txn, &KEY_CONSUMER_FILE)?;
+    pub fn new(env: &'env Env, name: &str, chunks_to_keep: Option<u64>) -> Result<Self, Box<dyn Error>> {
+        let mut txn = env.write_txn()?;
+        let producer_db: Database<U64<BE>, U64<BE>> = env.db(&mut txn, &format!("{}_{}", name, "producer"))?;
+        let consumer_db: Database<Str, U64<BE>> = env.db(&mut txn, &format!("{}_{}", name, "consumer"))?;
+
+        let file_num = consumer_db.get(&txn, &KEY_CONSUMER_FILE)?.unwrap();
+        let bytes_read = consumer_db.get(&txn, &KEY_CONSUMER_BYTES_READ)?.unwrap();
         txn.commit()?;
 
         let mut reader = Reader::new(&env.root, name, file_num)?;
@@ -157,11 +132,11 @@ impl <'env> Consumer<'env> {
             reader.set_bytes_read(bytes_read)?;
         }
 
-        Ok(Consumer { env, db, reader, chunks_to_keep: chunks_to_keep.unwrap_or(8) })
+        Ok(Consumer { env, producer_db, consumer_db, reader, chunks_to_keep: chunks_to_keep.unwrap_or(8) })
     }
 
-    pub fn pop_front_n(&mut self, n: u64) -> Result<Vec<Item>, anyhow::Error> {
-        let mut txn: RwTransaction<'_> = self.env.transaction_rw()?;
+    pub fn pop_front_n(&mut self, n: u64) -> Result<Vec<Item>, Box<dyn Error>> {
+        let mut txn = self.env.write_txn()?;
         self.check_chunks_to_keep(&mut txn)?;
 
         let mut items = vec![];
@@ -183,28 +158,25 @@ impl <'env> Consumer<'env> {
             }
         }
 
-        self.inc(&mut txn, &KEY_CONSUMER_OFFSET, delta)?;
-        self.replace(&mut txn, &KEY_CONSUMER_BYTES_READ, self.reader.get_bytes_read())?;
+        self.inc_offset(&mut txn, delta)?;
         txn.commit()?;
         Ok(items)
     }
 
-    pub fn pop_front(&mut self) -> Result<Option<Item>, anyhow::Error> {
-        let mut txn = self.env.transaction_rw()?;
+    pub fn pop_front(&mut self) -> Result<Option<Item>, Box<dyn Error>> {
+        let mut txn = self.env.write_txn()?;
         self.check_chunks_to_keep(&mut txn)?;
 
         match self.reader.read() {
             Ok(item) => {
-                self.inc(&mut txn, &KEY_CONSUMER_OFFSET, 1)?;
-                self.replace(&mut txn, &KEY_CONSUMER_BYTES_READ, self.reader.get_bytes_read())?;
+                self.inc_offset(&mut txn, 1)?;
                 txn.commit()?;
                 return Ok(Some(item));
             },
             Err(_) => {
                 if self.rotate(&mut txn)? {
                     let item = self.reader.read()?;
-                    self.inc(&mut txn, &KEY_CONSUMER_OFFSET, 1)?;
-                    self.replace(&mut txn, &KEY_CONSUMER_BYTES_READ, self.reader.get_bytes_read())?;
+                    self.inc_offset(&mut txn, 1)?;
                     txn.commit()?;
                     return Ok(Some(item));
                 } else {
@@ -215,9 +187,17 @@ impl <'env> Consumer<'env> {
         }
     }
 
-    fn check_chunks_to_keep(&mut self, txn: &mut RwTransaction) -> Result<(), anyhow::Error> {
-        let head = Self::get_value(self.db, txn, &KEY_CONSUMER_FILE)?;
-        let (tail, _) = Self::get_tail(self.db, txn)?;
+    fn inc_offset(&mut self, txn: &mut RwTxn, delta: u64) -> Result<(), Box<dyn Error>> {
+        let offset = self.consumer_db.get(&txn, &KEY_CONSUMER_OFFSET)?.unwrap();
+        self.consumer_db.put(txn, &KEY_CONSUMER_OFFSET, &(offset + delta))?;
+
+        self.consumer_db.put(txn, &KEY_CONSUMER_BYTES_READ, &self.reader.get_bytes_read())?;
+        Ok(())
+    }
+
+    fn check_chunks_to_keep(&mut self, txn: &mut RwTxn) -> Result<(), Box<dyn Error>> {
+        let head = self.consumer_db.get(&txn, &KEY_CONSUMER_FILE)?.unwrap();
+        let (tail, _) = self.producer_db.iter(&txn)?.last().transpose()?.unwrap();
         let chunk_to_remove: i64 = tail as i64 + 1 - head as i64 - self.chunks_to_keep as i64;
         for _ in 0..chunk_to_remove {
             self.rotate(txn)?;
@@ -226,15 +206,15 @@ impl <'env> Consumer<'env> {
         Ok(())
     }
 
-    fn rotate(&mut self, txn: &mut RwTransaction) -> Result<bool, anyhow::Error> {
-        let head = Self::get_value(self.db, txn, &KEY_CONSUMER_FILE)?;
-        let (tail, _) = Self::get_tail(self.db, txn)?;
+    fn rotate(&mut self, txn: &mut RwTxn) -> Result<bool, Box<dyn Error>> {
+        let head = self.consumer_db.get(&txn, &KEY_CONSUMER_FILE)?.unwrap();
+        let (tail, _) = self.producer_db.iter(&txn)?.last().transpose()?.unwrap();
         if tail > head {
             self.reader.rotate()?;
-            txn.del(self.db, &u64_to_bytes(head), None)?;
-            self.replace(txn, &KEY_CONSUMER_FILE, head + 1)?;
-            self.replace(txn, &KEY_CONSUMER_OFFSET, 0)?;
-            self.replace(txn, &KEY_CONSUMER_BYTES_READ, 0)?;
+            self.producer_db.delete(txn, &head)?;
+            self.consumer_db.put(txn, &KEY_CONSUMER_FILE, &(head + 1))?;
+            self.consumer_db.put(txn, &KEY_CONSUMER_OFFSET, &0)?;
+            self.consumer_db.put(txn, &KEY_CONSUMER_BYTES_READ, &0)?;
             Ok(true)
         } else {
             Ok(false)
